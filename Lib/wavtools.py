37aaad2a7908a3bef5f79e5c68e251177413d085
erose <emoran522@gmail.com>
2024-04-28 16:36:19 -0400

    check metadata

--- Lib/wavtools.py
+++ Lib/wavtools.py
@@ -3,38 +3,25 @@ from pydub import AudioSegment
 from scipy.io import wavfile
 import numpy as np
 
-#check file extension
-#incase they didn't read my instructions I worked hard on
-def checkType(file):
-    fileExtension = os.path.splitext(file)[1]
-    if fileExtension == ".WAV":
-        return
-    elif fileExtension == ".mp3":
-        t = AudioSegment.from_mp3(file)
-        t.export(file, format='wav')
-        print(t)
-        return
-    elif fileExtension == ".aac":
-        t = AudioSegment.from_file(file, format='aac') #needs to be changed to aac not mp3
-        t.export(file, format='wav')
-        print(t)
-        return
-    else:
-        print("Wrong file type")
-        return
+
 
 
 #metadata related to the sound file
-def checkMetaData(file):
-    audio = AudioSegment.from_file(file)
+def checkMetaData(file_path):
+    try:
+        audio = AudioSegment.from_file(file_path)
 
-    print("Audio Info:")
-    print(f"Channels: {audio.channels}")
-    print(f"Sample Width: {audio.sample_width} bytes")
-    print(f"Frame Rate: {audio.frame_rate} Hz")
-    print(f"Frame Width: {audio.frame_width} bytes")
-    print(f"Length: {len(audio)} milliseconds")
-    return len(audio)
+        print("Audio Info:")
+        print(f"Channels: {audio.channels}")
+        print(f"Sample Width: {audio.sample_width} bytes")
+        print(f"Frame Rate: {audio.frame_rate} Hz")
+        print(f"Frame Width: {audio.frame_width} bytes")
+        print(f"Length: {len(audio)} milliseconds")
+
+        return audio.channels, audio.sample_width, audio.frame_rate, audio.frame_width, len(audio)
+    except Exception as e:
+        print(f"Error checking metadata: {e}")
+        return None
 
 
 #converts to monophonic
